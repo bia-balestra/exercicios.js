7aa6e559d659b0fad87021b1943bb0b186eeb2ad
biabalestra <118934947+biabalestra@users.noreply.github.com>
2023-02-21 16:56:27 -0300

    calc

--- exercicios.js
+++ exercicios.js
@@ -1,9 +1,9 @@
-let info = {
-personagem: 'Margarida',
-origem: 'Pato Donald',
-nota: 'namorada do personagem principal',
-recorrente: 'não',
-} 
+// let info = {
+// personagem: 'Margarida',
+// origem: 'Pato Donald',
+// nota: 'namorada do personagem principal',
+// recorrente: 'não',
+// } 
 
 
 // console.log(`Bem-vinda, ${info.personagem}`)
@@ -19,37 +19,111 @@ recorrente: 'não',
 // }
 
 
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: 'Christmas on Bear Mountain',
-    nota: 'Último McPatinhas',
-    recorrente: 'sim'
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: 'Christmas on Bear Mountain',
+//     nota: 'Último McPatinhas',
+//     recorrente: 'sim'
+// }
+
+
+// for (let prop  in info2) {
+//     if (prop !== 'recorrente') {
+//         console.log(`${prop} : ${info2[prop]} e ${info[prop]}`)
+//     } else {
+//         if (info[prop] === 'sim'&& info2[prop] === 'sim'){
+//             console.log('recorrente: ambos recorrentes')
+//         }
+//         else if (info[prop] === 'sim' && info2[prop] === 'não'){
+//             console.log('Margarida recorrente e Tio Patinhas não recorrente');
+//         }
+//         else if (info[prop] === 'não' && info2[prop] === 'sim'){
+//             console.log('Margarida não recorrente e Tio Patinhas recorrente');
+//         }else{
+//             console.log('ambos não recorrentes');
+//         }
+
+//     }
+    
+// }
+
+
+
+
+
+//  let  leitor = {
+//     nome: 'Julia',
+//     sobrenome: 'Pessoa',
+//     idade: 21,
+//     livrosFavoritos:[
+//         {
+//         titulo: 'O Pior Dia De Todos',
+//         autor: 'Daniela Kopsch',
+//         editora:'Tordesilhas',
+//         },
+//         {
+//             titulo:'O Amor Não é Obvio',
+//             autor:'Elayne Baeta',
+//             editora:'Galera',
+//         }
+//     ]
+    
+// }
+ 
+
+// console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama ${leitor.livrosFavoritos[0].titulo}`)
+
+
+//  console.log(`${leitor.nome} tem ${leitor.livrosFavoritos.length} livros favoritos`)
+
+
+ 
+
+
+let order = {
+    name: 'Rafael Andrade',
+    phoneNumber: '11-98763-1416',
+    address: {
+      street: 'Rua das Flores',
+      number: '389',
+      apartment: '701',
+    },
+    order:{
+        pizza:{
+            marguerita: {
+                amount: 1,
+                price: 25,
+            },
+            peperoni:{
+                amount:1,
+                price:20,
+            }
+        },
+        drinks:{
+            coke:{
+                type:'Coca-Cola Zero',
+                price: 10,
+                amount: 1,
+            }
+        },
+        delivery:{
+            deliveryPerson: 'Ana Silveira',
+            price: 5,
+        },
+        payment:{
+            total:60
+        }
+    }
 }
 
+function custumerInfo(order) {
+}
 
-for (let prop  in info2) {
-    if (prop !== 'recorrente') {
-        console.log(`${prop} : ${info2[prop]} e ${info[prop]}`)
-    } else {
-        if (info[prop] === 'sim'&& info2[prop] === 'sim'){
-            console.log('recorrente: ambos recorrentes')
-        }
-        else if (info[prop] === 'sim' && info2[prop] === 'não'){
-            console.log('Margarida recorrente e Tio Patinhas não recorrente');
-        }
-        else if (info[prop] === 'não' && info2[prop] === 'sim'){
-            console.log('Margarida não recorrente e Tio Patinhas recorrente');
-        }else{
-            console.log('ambos não recorrentes');
-        }
+custumerInfo(order)
 
-    }
+
+function order(order) {
     
 }
-
-
-
-
-
 
 
---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+function Calcular(n1, n2) {
+    n1 = parseFloat (document.getElementById('n1').value)
+    n2 = parseFloat (document.getElementById('n2').value)
+    console.log(n1 + n2)
+
+    selector = document.getElementById('selector').value
+
+    switch (selector) {
+        case '+':
+            calculo = (n1 + n2)
+            document.getElementById('resultado').innerHTML = `O resultado de ${n1} + ${n2} é = ${calculo}`
+            break;
+
+            case '-':
+            calculo = (n1 - n2)
+            document.getElementById('resultado').innerHTML = `O resultado de ${n1} - ${n2} é = ${calculo}`
+            break;
+    
+            case '*':
+                calculo = (n1 * n2)
+                document.getElementById('resultado').innerHTML = `O resultado de ${n1} * ${n2} é = ${calculo}`
+                break;
+
+            case '/':
+                calculo = (n1 / n2)
+                document.getElementById('resultado').innerHTML = `O resultado de ${n1} / ${n2} é = ${calculo}`
+                break;
+    }
+    
+}
